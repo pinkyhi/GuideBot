919136752fdb6700bb9c00056f73d1d2c4a0c459
Ivan <43240272+pinkyhi@users.noreply.github.com>
2020-10-04 19:29:17 +0300

    First entity

---
+++ GuideBot.DAL/Entities/BaseEntities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuideBot.DAL.Entities.BaseEntities
+{
+    public abstract class BaseEntity : BaseDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ GuideBot.DAL/Entities/Guide.cs
@@ -0,0 +1,16 @@
+﻿using GuideBot.DAL.Entities.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuideBot.DAL.Entities
+{
+    public class Guide : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string Descripton { get; set; }
+
+        public string Requisites { get; set; }
+    }
+}
